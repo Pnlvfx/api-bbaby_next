dccc86919cc03eba3085e0b89f91fb9cb73563d0
simone <simonegauli@gmail.com>
2022-10-15 14:31:25 +0200

    new

--- components/music/musicCtrl.ts
+++ components/music/musicCtrl.ts
@@ -11,11 +11,11 @@ const musicCtrl = {
     search: async (req: Request, res: Response) => {
         try {
             const {text} = req.body;
-            const path = coraline.use('puppeteer');
             const browser = await puppeteer.launch({
                 args: ['--no-sandbox', '--disabled-setupid-sandbox']
             });
             const page = await browser.newPage();
+            await page.waitForSelector('#container')
             const url = `https://www.youtube.com/results?search_query=${text}`;
             await page.goto(url);
             const data = await page.evaluate(() =>
